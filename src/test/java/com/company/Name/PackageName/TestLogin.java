b2ccf9a6d2a7bde1588185ebe16d7b7e9368a5d6
shuvendupanda1 <shuvendupanda22@gmail.com>
2020-05-18 16:32:59 +0530

    first commite login test rs compoments

---
+++ src/test/java/com/company/Name/PackageName/TestLogin.java
@@ -0,0 +1,19 @@
+package com.company.Name.PackageName;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestLogin {
+	
+	@Test
+	public void test(){
+		System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.google.com/");
+		
+	}
+
+}

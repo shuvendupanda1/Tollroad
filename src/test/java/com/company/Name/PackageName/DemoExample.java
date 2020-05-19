05918622ead156eba8affa695f57ba4b578782af
shuvendupanda1 <shuvendupanda22@gmail.com>
2020-05-19 17:24:55 +0530

    add demo class

---
+++ src/test/java/com/company/Name/PackageName/DemoExample.java
@@ -0,0 +1,12 @@
+package com.company.Name.PackageName;
+
+public class DemoExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HEllo");
+
+	}
+
+}

a0b2d7991d320684e568ca70955dd18a57f289ac
shuvendupanda1 <shuvendupanda22@gmail.com>
2020-05-19 17:34:12 +0530

    Second Test

--- src/test/java/com/company/Name/PackageName/TestLogin.java
+++ src/test/java/com/company/Name/PackageName/TestLogin.java
@@ -16,6 +16,7 @@ public class TestLogin {
 		System.out.println(title);
 		Assert.assertEquals(title, "Google");
 		System.out.println("*****END TEST*****");
+		System.out.println("*******************");
 	}   
 }
 

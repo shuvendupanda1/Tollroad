c05e1f0b7b0d083fb46a9809cbd77dde52dad8eb
shuvendupanda1 <shuvendupanda22@gmail.com>
2020-05-20 23:15:30 +0530

    Up date the product details page

---
+++ src/test/java/com/company/Name/PackageName/ProductDetailsPage.java
@@ -0,0 +1,11 @@
+package com.company.Name.PackageName;
+
+public class ProductDetailsPage {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("*****Product Details Page*****");
+		
+	}
+
+}

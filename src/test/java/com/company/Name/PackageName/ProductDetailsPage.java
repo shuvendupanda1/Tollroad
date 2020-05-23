6f5de46851a74e904df74131d172574dc8f9d53f
shuvendupanda1 <shuvendupanda22@gmail.com>
2020-05-23 22:47:15 +0530

    BYE

--- src/test/java/com/company/Name/PackageName/ProductDetailsPage.java
+++ src/test/java/com/company/Name/PackageName/ProductDetailsPage.java
@@ -12,8 +12,7 @@ public class ProductDetailsPage {
 		WebDriver driver=new ChromeDriver();
 		driver.get("www.google.com");
 		System.out.println("HELLO JAVA TEST RUNNIMG");
-		
-		
+		System.out.println("BYE");
 	}
 
 }

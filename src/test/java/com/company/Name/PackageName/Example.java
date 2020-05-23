6042000b1accd5999736b689e46b29d600e499f2
shuvendupanda1 <36598483+shuvendupanda1@users.noreply.github.com>
2020-05-23 18:15:59 +0530

    Update Example.java

--- src/test/java/com/company/Name/PackageName/Example.java
+++ src/test/java/com/company/Name/PackageName/Example.java
@@ -6,10 +6,5 @@ public class Example {
 	public static void main(String[] args) {
 		System.out.println("TEST JAVA");
 		System.out.println("TEST JAVA");
-		System.out.println("TEST JAVA");
-		System.out.println("TEST JAVA");
-		System.out.println("TEST JAVA");
-		System.out.println("TEST JAVA");
-		System.out.println("TEST JAVA");
 	}
 }

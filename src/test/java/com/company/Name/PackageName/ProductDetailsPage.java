9fe10ed2bd47bf57d1e25bd1a19a577cee334136
shuvendupanda1 <shuvendupanda22@gmail.com>
2020-05-23 20:32:47 +0530

    RAW

--- src/test/java/com/company/Name/PackageName/ProductDetailsPage.java
+++ src/test/java/com/company/Name/PackageName/ProductDetailsPage.java
@@ -1,10 +1,17 @@
 package com.company.Name.PackageName;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ProductDetailsPage {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("*****Product Details Page*****");
+		System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("www.google.com");
+		
 		
 	}
 
